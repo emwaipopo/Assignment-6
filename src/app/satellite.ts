5852f810bda3d3d974a49edb884bf781a9e2b27d
emwaipopo <emmanuel_mwaipopo@cable.comcast.com>
2020-04-22 12:35:03 -0400

    created orbit-list component and fetch data from external source

---
+++ src/app/satellite.ts
@@ -0,0 +1,18 @@
+import { observable } from 'rxjs';
+import { ThrowStmt } from '@angular/compiler';
+
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+        this.name = name;
+        this.orbitType = orbitType;
+        this.type = type;
+        this.operational = operational;
+        this.launchDate = launchDate;
+    }
+}
